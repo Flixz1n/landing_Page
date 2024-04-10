4658f6012ba7a0b0f111333429876ac5fa0fafad
Lucas <lucas.flixzin@gmail.com>
2024-04-10 14:31:30 -0300

    ajustes extras

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,16 +2,6 @@ const menuToggle = document.querySelector('.toggle');
 const showcase = document.querySelector('.showcase');
 
 menuToggle.addEventListener('click', () => {
-  menuToggle.classList.toggle('active');
+  menuToggle.classList.toggle('--active');
   showcase.classList.toggle('active');
 })
-
-// function ocultaElementos() {
-//   const header = document.querySelector('header');
-//   header.classList.add('header--is-hidden');
-// }
-
-// function exibeElementosHeader() {
-//   const header = document.querySelector('header');
-//   header.classList.remove('header--is-hidden');
-// }
