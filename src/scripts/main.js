3993ecef71e4d3e3e4db2d738873a7a4fab8827e
Lucas <lucas.flixzin@gmail.com>
2024-04-10 14:21:59 -0300

    ajeitando o menu e mudança de cor nos textos

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,3 +5,13 @@ menuToggle.addEventListener('click', () => {
   menuToggle.classList.toggle('active');
   showcase.classList.toggle('active');
 })
+
+// function ocultaElementos() {
+//   const header = document.querySelector('header');
+//   header.classList.add('header--is-hidden');
+// }
+
+// function exibeElementosHeader() {
+//   const header = document.querySelector('header');
+//   header.classList.remove('header--is-hidden');
+// }
